58bfabbbd8177f694fb55f13ee4d4acbdca2b843
tungp55 <tungcntt55@gmail.com>
2023-02-23 10:53:04 +0700

    add heroku

--- src/__init__.py
+++ src/__init__.py
@@ -7,29 +7,32 @@ from src.auth import auth
 from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 import os
 
-app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY=os.environ.get("SECRET_KEY"),
-    SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
-    SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS=False,
-    JWT_SECRET_KEY= os.environ.get("JWT_SECRET_KEY")
-)
 
-db.app = app
-db.init_app(app)
-JWTManager(app)
-app.register_blueprint(auth)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY"),
+        SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+        SQLALCHEMY_DATABASE_TRACK_MODIFICATIONS=False,
+        JWT_SECRET_KEY= os.environ.get("JWT_SECRET_KEY")
+    )
 
-app.register_blueprint(users)
+    db.app = app
+    db.init_app(app)
+    JWTManager(app)
+    app.register_blueprint(auth)
+
+    app.register_blueprint(users)
 
 
 
 
-@app.errorhandler(HTTP_404_NOT_FOUND)
-def handler_404(e):
-    return jsonify({'error': 'Khong tim thay'}), HTTP_404_NOT_FOUND
+    @app.errorhandler(HTTP_404_NOT_FOUND)
+    def handler_404(e):
+        return jsonify({'error': 'Khong tim thay'}), HTTP_404_NOT_FOUND
 
-@app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
-def handler_500(e):
-    return jsonify({'error': 'Co loi da xay ra'}), HTTP_500_INTERNAL_SERVER_ERROR
+    @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+    def handler_500(e):
+        return jsonify({'error': 'Co loi da xay ra'}), HTTP_500_INTERNAL_SERVER_ERROR
 
+    return app
